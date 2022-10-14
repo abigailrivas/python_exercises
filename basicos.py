25b9268a8c6caedd2d7b1cf4fdd376f8ea152889
Abi Rivas <lourdes.2502@gmail.com>
2022-10-14 20:33:15 -0300

    Termine ejercicio 1

---
+++ basicos.py
@@ -0,0 +1,10 @@
+#Ejercicio 1
+
+name = input("What's your name?: ")
+age = input("What's your age?: ")
+age = int(age)
+aging = str(age + 100)
+
+print("Hi " + name + " in 100 years you'll turn " + aging)
+
+#Ejercicio 2
