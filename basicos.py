7f810113c273d212bbfcc2ba32e2e7637d824e35
Abi Rivas <lourdes.2502@gmail.com>
2022-10-15 10:17:37 -0300

    Ejercicios 2 y 3

--- basicos.py
+++ basicos.py
@@ -8,3 +8,16 @@ aging = str(age + 100)
 print("Hi " + name + " in 100 years you'll turn " + aging)
 
 #Ejercicio 2
+number = int(input("Hi, enter a number :"))
+
+if number % 2 == 0:
+    print("Your number is even!")
+else:
+    print("Your number is odd")
+
+#Ejercicio 3
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for x in a:
+    if x < 5:
+        print(x)
+
